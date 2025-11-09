3daaafc8a39d600a4bdcef17033538067e456402
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-11-09 17:05:59 +0530

    update - all functioning done

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Result from './Result'
 import './App.css'
 
 function App() {
-
+  const [operand, setOperand] = useState('');
   const [result, setResult] = useState('0');
 
   return (
-    <>
+    <div className='calculator '>
       <Result result={result} />
-      <Input result={result} setResult={setResult} />
-    </>
+      <Input result={result} setResult={setResult} setOperand={setOperand} operand={operand} />
+    </div>
   )
 }
 
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,28 +1,56 @@
-import Button from "./Button";
 
-function Input({ result, setResult }) {
+function Input({ result, setResult, operand, setOperand }) {
 
+    const isOperation = (value) => {
+        return value === '+' ||
+            value === '-' ||
+            value === '/' ||
+            value === '*' ||
+            value === '='
+    }
+
+    const validate = (currentValue) => {
+        // Why? : becuase like 02 makes problem in eval.
+        if (result === '0' && isOperation(currentValue)) return;
+
+        const previousValue = result.charAt(result.length - 1);
+        if (previousValue === '.' && currentValue === '.') return;
+        if (isOperation(previousValue) && isOperation(currentValue)) return;
+
+        if (operand.includes('.') && currentValue === '.') return;
+        return true;
+    }
+
+    function handleClick(currentValue) {
+
+        if (!validate(currentValue)) return;
+
+        if (isOperation(currentValue)) setOperand('');
+        else setOperand(operand + currentValue);
+
+        if (currentValue === '=') {
+            const finalResult = eval(result).toString();
+            setResult(finalResult);
+            console.log(finalResult);
+            setOperand(finalResult);
+        } else if (currentValue === 'C') {
+            setResult('0');
+            setOperand('0');
+        } else {
+            if (result === '0') setResult(currentValue);
+            else setResult(result + currentValue);
+        }
+    }
+
+    const buttonValues = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '0', '.', 'C', '/', '=']
 
     return (
-        <div className="input-container">
-            <Button result={result} setResult={setResult} value={'1'} />
-            <Button result={result} setResult={setResult} value={'2'} />
-            <Button result={result} setResult={setResult} value={'3'} />
-            <Button result={result} setResult={setResult} value={'4'} />
-            <Button result={result} setResult={setResult} value={'5'} />
-            <Button result={result} setResult={setResult} value={'6'} />
-            <Button result={result} setResult={setResult} value={'7'} />
-            <Button result={result} setResult={setResult} value={'8'} />
-            <Button result={result} setResult={setResult} value={'9'} />
-            <Button result={result} setResult={setResult} value={'0'} />
-            <Button result={result} setResult={setResult} value={'.'} />
-            <Button result={result} setResult={setResult} value={'+'} />
-            <Button result={result} setResult={setResult} value={'-'} />
-            <Button result={result} setResult={setResult} value={'*'} />
-            <Button result={result} setResult={setResult} value={'/'} />
-            <Button result={result} setResult={setResult} value={'C'} />
-            <Button result={result} setResult={setResult} value={'='} />
-
+        <div className="input-container ">
+            {buttonValues.map((value, index) => {
+                return <button key={index} onClick={() => {
+                    handleClick(value);
+                }} value={value}>{value}</button>
+            })}
         </div>
     )
 }
