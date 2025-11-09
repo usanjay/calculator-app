25831c4d1b257a81a84570d52c6338eea568bc74
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-11-10 02:00:12 +0530

    formatting

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,7 +1,8 @@
 export default function Result({ result }) {
     return (
-        <div className="bg-yellow-100 p-2 px-4 text-3xl mt-3 rounded-md inset-shadow-sm
-        inset-shadow-yellow-300 text-right overflow-hidden whitespace-nowrap
+        <div className="
+          bg-yellow-100 p-2 px-4 text-3xl mt-3 rounded-md inset-shadow-sm
+          inset-shadow-yellow-300 text-right overflow-hidden whitespace-nowrap
         ">{result}</div>
     )
 }
